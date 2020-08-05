2750426279bb13c396aa87d09b94c70ce1b21829
gavacasv <58041959+gavacasv@users.noreply.github.com>
2020-08-05 15:28:22 -0500

    Add files via upload

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -19,39 +19,17 @@ public class Main {
              System.out.print(" " +array[i]);
         }
          System.out.println("");
-         sortStringArray(t);
+         
          
          
          for(int i=0; i<t.length; i++){
              System.out.print(" " +t[i]);
         }
     }
-    public static int indexOfSmallestFrom(int[] array, int startIndex){
-        int index=startIndex;
-        int aux=array[index];
-        for(int i=startIndex; i<array.length; i++){
-            
-            if( aux>array[i]){
-                aux=array[i];
-                index=i;
-            }
-        }
+   
 
-        return index;
-    }
-    
-    public static void swap(int array[], int index1, int index2){
-        int auxiliar = array[index1];
-        array[index1]=array[index2];
-        array[index2]=auxiliar;
-    }
     public static void sort(int[] array){
-        int i=0;
-        while (i<array.length-1){
-            int auxIndex=indexOfSmallestFrom(array,i);
-            swap(array,i,auxIndex);
-            i++;
-        }
+        Arrays.sort(array);
     }
     
    
@@ -59,10 +37,7 @@ public class Main {
         Collections.sort(integers);
     }
      
-    
-
-    
-    public static void sortStringArray(String[] array){
+    public static void sort(String[] array){
         Arrays.sort(array);
           
     }
